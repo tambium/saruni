5d8190db7992c537e18f1bfe3e2f362df9107009
Endre Vegh <endre.vegh@yahoo.com>
2020-05-26 17:48:17 +0200

    add shared babel config

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: true,
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3.6,
+          proposals: true,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+
+    [
+      "@babel/plugin-transform-modules-commonjs",
+      {
+        allowTopLevelThis: true,
+      },
+    ],
+  ],
+};
