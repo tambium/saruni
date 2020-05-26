110eda0c5e023fc2e2cd2692640bd5c2a4cdf412
Endre Vegh <endre.vegh@yahoo.com>
2020-05-26 17:53:52 +0200

    add create -keys command

---
+++ packages/cli/src/commands/create-key.ts
@@ -0,0 +1,44 @@
+import chalk from "chalk";
+import execa from "execa";
+import fs from "fs-extra";
+
+import { projectPath } from "@saruni/utils";
+
+interface CreateKeyParams {
+  name: string;
+}
+
+export const command = "create-key <name>";
+
+export const desc = "creates a key with aws that can be used in ssh sessions";
+
+export const handler = async (args: CreateKeyParams) => {
+  try {
+    // @ts-ignore
+    const hasKey = (await fs.exists("bastion-key.pem")) as boolean;
+
+    if (hasKey) {
+      console.log(chalk.red("The file bastion.key.pem already exists."));
+      console.log(
+        chalk.yellow(
+          "It is advised to backup this file then either delete or remove it."
+        )
+      );
+
+      process.exit(1);
+    }
+
+    const { stdout } = await execa("aws", [
+      "ec2",
+      "create-key-pair",
+      "--key-name",
+      "bastion-key",
+    ]);
+
+    const { KeyMaterial } = JSON.parse(stdout);
+
+    await fs.writeFile("bastion-key.pem", KeyMaterial);
+  } catch (e) {
+    console.log(e);
+  }
+};
