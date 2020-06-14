a4357083105669d2ea4962a56b6cd4869f4eacee
Endre Vegh <endre.vegh@yahoo.com>
2020-06-14 09:04:24 +0200

    remove cd as it does not have an effect on concurrent

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -1,15 +1,10 @@
 import concurrently from "concurrently";
-import execa from "execa";
-
-import { getPaths } from "@saruni/internal";
 
 export const command = "dev";
 
 export const desc = "Start development servers.";
 
 export const handler = async () => {
-  await execa("cd", [getPaths().base]);
-
   await concurrently([
     {
       command: "yarn ds",
