c16a8e9f2b1b8dd367bb647899159ceeda2fcc8e
Endre Vegh <endre.vegh@yahoo.com>
2020-06-14 08:55:26 +0200

    add prisma script

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -1,16 +1,24 @@
 import concurrently from "concurrently";
+import execa from "execa";
+
+import { getPaths } from "@saruni/internal";
 
 export const command = "dev";
 
 export const desc = "Start development servers.";
 
 export const handler = async () => {
+  await execa("cd", [getPaths().base]);
+
   await concurrently([
     {
       command: "yarn ds",
     },
     {
       command: "cd packages/web && yarn dev",
+    },
+    {
+      command: "cd packages/api && npx prisma generate --watch",
     },
   ]);
 };
