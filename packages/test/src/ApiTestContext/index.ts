9a50a218c49758c101ad347047cb0b928743f957
Endre Vegh <endre.vegh@yahoo.com>
2020-07-18 18:59:08 +0200

    add test context

---
+++ packages/test/src/ApiTestContext/index.ts
@@ -0,0 +1,75 @@
+import { makeExecutableSchema } from "@saruni/api";
+import { getPaths } from "@saruni/internal";
+import { graphql, ExecutionResult } from "graphql";
+import { ExecutionResultDataDefault } from "graphql/execution/execute";
+import path from "path";
+import type { PrismaClient } from "@prisma/client";
+
+const babelRequireHook = require("@babel/register");
+
+babelRequireHook({
+  extends: path.join(getPaths().api.base, ".babelrc.js"),
+  extensions: [".js", ".ts"],
+  only: [path.resolve(getPaths().api.graphql)],
+  ignore: ["node_modules"],
+  cache: false,
+});
+
+const { resolvers, typeDefs } = require(getPaths().api.graphql);
+
+interface TestContext {
+  db: PrismaClient;
+  executeGraphql: (
+    source: string,
+    options?: {
+      variables: any;
+      context: any;
+    }
+  ) => Promise<ExecutionResult<ExecutionResultDataDefault>>;
+  setGraphQLContext: (context: any) => void;
+}
+
+export const createTestContext = (db: PrismaClient): TestContext => {
+  let mainContext = {};
+
+  let schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  beforeAll(async () => {});
+
+  afterAll(async () => {
+    await db.disconnect();
+  });
+
+  async function executeGraphql(
+    source: string,
+    options?: { variables: any; context: any }
+  ) {
+    let extendedOptions: { variableValues?: any; contextValue } = {
+      contextValue: mainContext,
+    };
+
+    if (options?.variables) {
+      extendedOptions.variableValues = options.variables;
+    }
+
+    if (options?.context) {
+      extendedOptions.contextValue = { ...mainContext, ...options.context };
+    }
+
+    const result = await graphql({
+      source,
+      schema,
+      ...extendedOptions,
+    });
+
+    return result;
+  }
+
+  return {
+    db,
+    executeGraphql,
+    setGraphQLContext: (ctx) => {
+      mainContext = ctx;
+    },
+  };
+};
--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,1 +1,3 @@
 export * from "jest";
+
+export * from "./ApiTestContext";
