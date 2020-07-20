c55efdb4cc98d5d24c1d09053e0dbdfc1978791a
Endre Vegh <endre.vegh@yahoo.com>
2020-07-20 16:45:10 +0200

    modify test script

--- packages/cli/src/commands/test.ts
+++ packages/cli/src/commands/test.ts
@@ -1,4 +1,18 @@
+import babelRequireHook from "@babel/register";
+import { getPaths } from "@saruni/internal";
+import execa from "execa";
 import { run } from "jest";
+import path from "path";
+
+babelRequireHook({
+  extends: path.join(getPaths().api.base, ".babelrc.js"),
+  extensions: [".js", ".ts"],
+  only: [path.resolve(getPaths().api.db)],
+  ignore: ["node_modules"],
+  cache: false,
+});
+
+process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
 
 export const command = "test";
 
@@ -6,7 +20,23 @@ export const desc = "Runs jest with the project based setup.";
 
 export const handler = async () => {
   try {
-    run([`--config=${require.resolve("@saruni/config")}`]);
+    const { db } = require(getPaths().api.db);
+
+    await db.queryRaw(`DROP SCHEMA IF EXISTS "public" CASCADE`);
+
+    await db.disconnect();
+
+    await execa("npx", ["prisma", "migrate", "up", "--experimental"], {
+      cwd: getPaths().api.base,
+      env: { DATABASE_URL: process.env.DATABASE_URL_TEST },
+    });
+
+    await execa("yarn", ["sr", "db", "seed"], {
+      cwd: getPaths().api.base,
+      env: { DATABASE_URL: process.env.DATABASE_URL_TEST },
+    });
+
+    await run();
   } catch (e) {
     console.log(e);
   }
