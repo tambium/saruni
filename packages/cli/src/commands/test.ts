7b8eb223899c075048d8bad90e6b3336051cdbab
Endre Vegh <endre.vegh@yahoo.com>
2020-07-22 19:15:36 +0200

    move out database reassign

--- packages/cli/src/commands/test.ts
+++ packages/cli/src/commands/test.ts
@@ -12,14 +12,14 @@ babelRequireHook({
   cache: false,
 });
 
-process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
-
 export const command = "test";
 
 export const desc = "Runs jest with the project based setup.";
 
 export const handler = async () => {
   try {
+    process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
+
     const { db } = require(getPaths().api.db);
 
     await db.queryRaw(`DROP SCHEMA IF EXISTS "public" CASCADE`);
