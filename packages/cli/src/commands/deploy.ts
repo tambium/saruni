238eb9d8a5bba2de418b24eb11516fe38d0a907f
Endre Vegh <endre.vegh@yahoo.com>
2020-08-18 21:43:51 +0200

    first draft of deployment

--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -1,16 +1,22 @@
 import { getPaths } from '@saruni/internal';
 import execa from 'execa';
+import path from 'path';
 import Listr from 'listr';
 import { CommandBuilder } from 'yargs';
 
 export const command = 'deploy';
 
 export const builder: CommandBuilder = (yargs) => {
-  return yargs.option('stage', {
-    default: 'dev',
-    type: 'string',
-    choices: ['dev', 'prod'],
-  });
+  return yargs
+    .option('stage', {
+      default: 'dev',
+      type: 'string',
+      choices: ['dev', 'prod'],
+    })
+    .option('service', {
+      default: 'graphql',
+      choices: ['resources', 'graphql', 'auth', 'web'],
+    });
 };
 
 export const desc = 'deploys the services found in the services folder';
@@ -18,79 +24,108 @@ export const desc = 'deploys the services found in the services folder';
 const saruniJson = require(getPaths().saruni);
 
 export const handler = async (args) => {
-  try {
-    await new Listr([
-      {
-        title: `Preparing frontend`,
-        task: async () =>
-          new Listr([
-            {
-              title: 'Clearing build directories',
-              task: async () => {
-                await execa('yarn', ['clean'], { cwd: getPaths().web.base });
+  const region = saruniJson.serverless[args.stage].region;
+  const profile = saruniJson.serverless[args.stage].awsProfile;
+
+  if (args.service === 'resources') {
+    await execa('sls', ['--aws-profile', profile, 'deploy'], {
+      cwd: path.join(getPaths().base, 'packages/api/src/resources'),
+      stdio: 'inherit',
+    });
+  }
+
+  if (args.service === 'graphql') {
+    await execa('sls', ['--aws-profile', profile, 'deploy'], {
+      cwd: path.join(getPaths().base, 'packages/api/src/services/graphql'),
+      stdio: 'inherit',
+    });
+  }
+
+  if (args.service === 'auth') {
+    await execa('sls', ['--aws-profile', profile, 'deploy'], {
+      cwd: path.join(getPaths().base, 'packages/api/src/services/auth'),
+      stdio: 'inherit',
+    });
+  }
+
+  if (args.service === 'web') {
+    try {
+      await new Listr([
+        {
+          title: `Preparing frontend`,
+          task: async () =>
+            new Listr([
+              // {
+              //   title: 'Clearing build directories',
+              //   task: async () => {
+              //     await execa('yarn', ['clean'], { cwd: getPaths().web.base });
+              //   },
+              // },
+              {
+                title: 'Building Next.js production build',
+                task: async () => {
+                  await execa('yarn', ['build'], {
+                    cwd: getPaths().web.base,
+                    env: { STAGE: args.stage },
+                  });
+                },
               },
-            },
-            {
-              title: 'Building Next.js production build',
-              task: async () => {
-                await execa('yarn', ['build'], {
-                  cwd: getPaths().web.base,
-                  env: { STAGE: args.stage },
-                });
+              {
+                title: 'Creating a static build',
+                task: async () => {
+                  await execa('yarn', ['export'], {
+                    cwd: getPaths().web.base,
+                    env: {
+                      STAGE: args.stage,
+                    },
+                  });
+                },
               },
-            },
-            {
-              title: 'Creating a static build',
-              task: async () => {
-                await execa('yarn', ['export'], {
-                  cwd: getPaths().web.base,
-                  env: {
-                    STAGE: args.stage,
-                  },
-                });
-              },
-            },
-            {
-              title: 'Uploading to S3',
-              task: async () => {
-                await execa(
-                  `AWS_PROFILE=${saruniJson.serverless[args.stage].awsProfile}`,
-                  [
+              {
+                title: 'Uploading to S3',
+                task: async () => {
+                  await execa(
                     'aws',
-                    's3',
-                    'sync',
-                    'out',
-                    `s3://${
-                      saruniJson.serverless[args.stage].frontendS3Bucket
-                    }`,
-                    '--delete',
-                  ],
-                  { cwd: getPaths().web.base },
-                );
+                    [
+                      's3',
+                      '--profile',
+                      saruniJson.serverless[args.stage].awsProfile,
+                      'sync',
+                      'out',
+                      `s3://${
+                        saruniJson.serverless[args.stage].frontendS3Bucket
+                      }`,
+                      '--delete',
+                    ],
+                    { cwd: getPaths().web.base },
+                  );
+                },
               },
-            },
-            {
-              title: 'Invalidating cloudfront cache',
-              task: async () => {
-                await execa(
-                  `AWS_PROFILE=${saruniJson.serverless[args.stage].awsProfile}`,
-                  [
-                    'aws',
-                    'cloudfront',
-                    'create-invalidation',
-                    '--distribution-id',
-                    'E1W56ST7R2SIRK',
-                    '--paths',
-                    '*',
-                  ],
-                  { cwd: getPaths().web.base },
-                );
-              },
-            },
-          ]),
-      },
-    ]).run();
-  } catch (e) {
-    console.log(e);
+              // {
+              //   title: 'Invalidating cloudfront cache',
+              //   task: async () => {
+              //     await execa(
+              //       `AWS_PROFILE=${
+              //         saruniJson.serverless[args.stage].awsProfile
+              //       }`,
+              //       [
+              //         'aws',
+              //         'cloudfront',
+              //         'create-invalidation',
+              //         '--distribution-id',
+              //         'E1W56ST7R2SIRK',
+              //         '--paths',
+              //         '*',
+              //       ],
+              //       { cwd: getPaths().web.base },
+              //     );
+              // },
+              // },
+            ]),
+        },
+      ]).run();
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
